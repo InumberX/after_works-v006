8b16f73dcb8f36231c20624f52348cd953723e57
InumberX <inumberx@gmail.com>
2023-06-12 19:19:43 +0900

    add:next js

---
+++ src/app/frontend/next.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: 'out',
+  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+    // importした画像の型定義設定を無効にする
+    disableStaticImages: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            // 圧縮無効
+            svgo: false,
+          },
+        },
+      ],
+    })
+    return config
+  },
+}
+
+module.exports = nextConfig
