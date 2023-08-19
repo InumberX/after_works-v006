3153f961271000f4b26acc3df39abc02def0a53d
InumberX <inumberx@gmail.com>
2023-08-19 11:05:08 +0900

    add:base breadcrumb component

--- src/app/frontend/src/app/blogs/_components/index.tsx
+++ src/app/frontend/src/app/blogs/_components/index.tsx
@@ -1,10 +1,18 @@
 import { PageTitle } from '~/components/ui/typographies/PageTitle'
 import { Contact } from '~/components/common/Contact'
+import { BaseBreadcrumb } from '~/components/ui/breadcrumbs/BaseBreadcrumb'
 
 export const Index = () => {
   return (
     <>
       <PageTitle title='ブログ' subTitle='Blog' />
+      <BaseBreadcrumb
+        infos={[
+          {
+            name: 'ブログ',
+          },
+        ]}
+      />
       <Contact />
     </>
   )
