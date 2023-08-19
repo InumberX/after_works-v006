c2ab34422c1aa3099e58bb6bbfc4e74606ef37a5
InumberX <inumberx@gmail.com>
2023-08-19 12:53:44 +0900

    add:ga

--- src/app/frontend/src/app/layout.tsx
+++ src/app/frontend/src/app/layout.tsx
@@ -1,11 +1,16 @@
 import '~/styles/common.scss'
 import { AppProvider } from '~/providers/AppProvider'
 import { LayoutDefault } from '~/layouts/Default'
+import { GoogleAnalytics } from '~/components/common/GoogleAnalytics'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     // eslint-disable-next-line react/no-unknown-property
     <html lang='jp' prefix='og: http://ogp.me/ns#'>
+      <head>
+        <GoogleAnalytics />
+      </head>
+
       <body>
         <AppProvider>
           <LayoutDefault>{children}</LayoutDefault>
---
+++ src/app/frontend/src/components/common/GoogleAnalytics/index.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useEffect } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
+import Script from 'next/script'
+import { GOOGLE_ANALYTICS_ID } from '~/config/env'
+import { isActiveGA, pageview } from '~/libs/ga/gtag'
+
+export const GoogleAnalytics = () => {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    if (!isActiveGA) {
+      return
+    }
+
+    const url = pathname + searchParams.toString()
+    pageview(url)
+  }, [pathname, searchParams])
+
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script id='gtag-init' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GOOGLE_ANALYTICS_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  )
+}
