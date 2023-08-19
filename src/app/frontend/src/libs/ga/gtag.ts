013f13a55413ab6142ec15541f31f298e98d9ac4
N/NE <35207269+InumberX@users.noreply.github.com>
2023-08-19 13:11:29 +0900

    add:ga (#20)

---
+++ src/app/frontend/src/libs/ga/gtag.ts
@@ -0,0 +1,9 @@
+import { GOOGLE_ANALYTICS_ID } from '~/config/env'
+
+export const isActiveGA = GOOGLE_ANALYTICS_ID !== ''
+
+export const pageview = (path: string) => {
+  window.gtag('config', GOOGLE_ANALYTICS_ID, {
+    page_path: path,
+  })
+}
