4e43cc9785ac617c65483b841e9e2880aecb952b
InumberX <inumberx@gmail.com>
2023-08-20 21:51:43 +0900

    add:not found page

---
+++ src/app/frontend/src/app/[...not-found]/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from 'next'
+import { AppHead } from '~/components/common/AppHead'
+import { Index } from './_components'
+import { SITE_URL } from '~/config/env'
+import { routes } from '~/config/routes'
+
+export const generateMetadata = (): Metadata => {
+  return AppHead({
+    title: 'お探しのページが見つかりませんでした',
+    canonical: `${SITE_URL}${routes.notFound.url({})}`,
+    robots: 'noindex, nofollow',
+  })
+}
+
+const NotFoundPage = () => {
+  return <Index />
+}
+
+export default NotFoundPage
