fb7fecba52e6e1bc8800596ab48951db4a050991
NiNE <35207269+InumberX@users.noreply.github.com>
2026-03-20 11:05:20 +0900

    fix: storybook postcss error (#192)
    * fix: storybook
    
    * fix: eslint config

--- src/app/frontend/.storybook/main.ts
+++ src/app/frontend/.storybook/main.ts
@@ -8,11 +8,7 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 const config: StorybookConfig = {
   stories: ['../**/*.stories.@(ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links'],
   framework: {
     name: '@storybook/react-vite',
     options: {
