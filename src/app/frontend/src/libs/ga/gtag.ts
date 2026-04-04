d383b10a0fd8f8aefc8068c023efbf108a572c88
InumberX <nine@afterworks.jp>
2026-04-04 10:12:36 +0900

    fix: upgrade typescript

--- src/app/frontend/src/libs/ga/gtag.ts
+++ src/app/frontend/src/libs/ga/gtag.ts
@@ -2,8 +2,17 @@ import { GOOGLE_ANALYTICS_ID } from '@/config/env'
 
 export const isActiveGA = GOOGLE_ANALYTICS_ID !== ''
 
+type GtagWindow = Window &
+  typeof globalThis & {
+    gtag: (
+      command: string,
+      action: string,
+      params: Record<string, unknown>,
+    ) => void
+  }
+
 export const pageview = (path: string) => {
-  window.gtag('config', GOOGLE_ANALYTICS_ID, {
+  ;(window as GtagWindow).gtag('config', GOOGLE_ANALYTICS_ID, {
     page_path: path,
   })
 }
